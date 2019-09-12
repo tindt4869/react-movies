10f5aa6e9850867928fba2ea12582a86408d753c
TinDT <tindt@gmail.com>
2019-09-12 22:39:04 +0700

    chore: remove unused component in test of App component

--- app/containers/App/tests/index.test.js
+++ app/containers/App/tests/index.test.js
@@ -2,15 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Route } from 'react-router-dom';
 
-import Header from 'components/Header';
 import App from '../index';
 
 describe('<App />', () => {
-  it('should render the header', () => {
-    const renderedComponent = shallow(<App />);
-    expect(renderedComponent.find(Header).length).toBe(1);
-  });
-
   it('should render some routes', () => {
     const renderedComponent = shallow(<App />);
     expect(renderedComponent.find(Route).length).not.toBe(0);
