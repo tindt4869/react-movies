255c78b8a063b7c4c7c2083fb8c3a2870ba43bc5
TinDT <tindt@gmail.com>
2019-09-12 23:29:23 +0700

    chore: make navbar 100% width

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -9,21 +9,23 @@ import './style.scss';
 import Navbar from 'components/Navbar';
 
 const App = () => (
-  <div className="app-wrapper">
-    <Helmet
-      titleTemplate="%s - Movie Info"
-      defaultTitle="Movie Info Application"
-    >
-      <meta name="description" content="Movies Info application" />
-    </Helmet>
+  <>
     <Navbar />
-    <Switch>
-      <Route exact path="/" component={PopularPage} />
-      <Route path="/popular" component={PopularPage} />
-      <Route path="/top-rate" component={TopRatePage} />
-      <Route path="" component={NotFoundPage} />
-    </Switch>
-  </div>
+    <div className="app-wrapper">
+      <Helmet
+        titleTemplate="%s - Movie Info"
+        defaultTitle="Movie Info Application"
+      >
+        <meta name="description" content="Movies Info application" />
+      </Helmet>
+      <Switch>
+        <Route exact path="/" component={PopularPage} />
+        <Route path="/popular" component={PopularPage} />
+        <Route path="/top-rate" component={TopRatePage} />
+        <Route path="" component={NotFoundPage} />
+      </Switch>
+    </div>
+  </>
 );
 
 export default App;
